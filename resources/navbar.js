205cecf2d1394f8c8979ac9005efc0dc08c95493
Sirius-Development <griffinferguson.us@gmail.com>
2021-08-22 11:31:44 -0500

    Rebrand RO0K site for Landstrider Legion

--- resources/navbar.js
+++ resources/navbar.js
@@ -1,22 +1,20 @@
 var nav = document.createElement('nav');
-nav.innerHTML =
-`
+nav.innerHTML = `
 <table>
 <tr>
    <th><a href="/index.html" class="img-link">
       <img src="/resources/icon.png" alt="logo" height="30px" />
-      The Rook Platoon</a>
+      Landstrider Legion</a>
    </th>
 </tr>
 </table>
 <table>
 <tr>
-   <th><a href="https://discord.gg/FZXrkQXJPm" target="_blank">Discord</a></th>
-   <th><a href="https://inara.cz/squadron/8829/" target="_blank">Inara</a></th>
-   <th><a href="https://www.edsm.net/en/guilds/summary/id/97/name/Rook+Platoon" target="_blank">EDSM</a></th>
+   <th><a href="#" target="_blank">Discord</a></th>
+   <th><a href="#" target="_blank">Inara</a></th>
+   <th><a href="#" target="_blank">EDSM</a></th>
    <th>|</th>
    <th><a href="/news/index.html">News &amp; Updates</a></th>
 </tr>
-</table>
-`
+</table>`
 document.body.append(nav);
