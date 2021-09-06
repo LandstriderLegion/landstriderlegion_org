c4792560552f4363c1ad059688742afd0374b202
Sirius-Development <griffinferguson.us@gmail.com>
2021-09-05 20:09:58 -0500

    update(styles): Stop unnecessary stylesheet loading, fix styles

--- resources/navbar.js
+++ resources/navbar.js
@@ -14,6 +14,7 @@ nav.innerHTML = `
    <th><a href="/links/redirect.html?q=inara" target="_blank">Inara</a></th>
    <th>|</th>
    <th><a href="/news/index.html">News &amp; Updates</a></th>
+   <th><a href="/events/index.html">Events</a></th>
 </tr>
 </table>`
 document.body.append(nav);
