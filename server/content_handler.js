02475a600702f3422a7ffa221d76b0fbc4aae53a
Griffin <griffinferguson.us@gmail.com>
2022-05-20 14:25:07 -0500

    server: update(script): Begin working on db connections

--- server/content_handler.js
+++ server/content_handler.js
@@ -1,13 +1,19 @@
 const http = require('http');
 const mongo = require('mongodb');
+const dbUrl = 'mongodb://localhost:5000/content';
 
 console.log('Content Handler running on port 3001');
 
-// Initialize content database
+// Connect content database
+const client = new mongo.MongoClient(dbUrl);
+var db = { 
+   events: client.collection('events'), 
+   news: client.collection('news')
+};
+
 
 http.createServer((req, res) => {
    res.writeHead(200, {'Content-type': 'application/json'});
-   res.write(req.url);
 
    if (req.url == '/' || req.url == '/news' || req.url == '/events') {
       res.write(`Please specify parameters`);
--- server/db.js
+++ server/db.js
@@ -1,32 +1,31 @@
-const cp = require('child_process')
-const mongo = require('mongodb')
-const dbUrl = 'mongodb://localhost:5000/'
+const mongo = require('mongodb');
+const dbUrl = 'mongodb://localhost:5000/';
 
 setTimeout(() => {
    // Content database
    mongo.MongoClient.connect(dbUrl, (err, db) => {
       if (err) throw err;
 
-      var dbo = db.db('content')
+      var dbo = db.db('content');
       console.log('Created content database');
       dbo.createCollection('news', (err, res) => {
-         if (err) throw err
-         console.log('Created news collection')
+         if (err) throw err;
+         console.log('Created news collection');
       })
       dbo.createCollection('events', (err, res) => {
-         if (err) throw err
-         console.log('Created events collection')
+         if (err) throw err;
+         console.log('Created events collection');
       })
       
-      dbo = db.db('data')
+      dbo = db.db('data');
       console.log('Created data database');
       dbo.createCollection('journals', (err, res) => {
-         if (err) throw err
-         console.log('Created journals collection')
+         if (err) throw err;
+         console.log('Created journals collection');
       })
       dbo.createCollection('bgs', (err, res) => {
-         if (err) throw err
-         console.log('Created bgs collection')
+         if (err) throw err;
+         console.log('Created bgs collection');
       })
 
    });
