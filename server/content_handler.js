b79f2c3962268a8c910a21288e6aeb822e6d9f14
Griffin <griffinferguson.us@gmail.com>
2022-06-15 11:16:57 -0500

    server: update(scripts): Work on templates

--- server/content_handler.js
+++ server/content_handler.js
@@ -1,6 +1,7 @@
 const http = require('http');
 const mongo = require('mongodb');
 const dbUrl = 'mongodb://localhost:5000/content';
+const pug = require("pug")
 
 console.log('Content Handler running on port 3001');
 
@@ -11,11 +12,17 @@ var db = {
    news: client.collection('news')
 };
 
+// Compile templates
+const templates = {
+   events: pug.compileFile('./templates/event.pug'),
+   news: pug.compileFile('./templates/news.pug')
+}
+
 
 http.createServer((req, res) => {
    res.writeHead(200, {'Content-type': 'application/json'});
 
-   if (req.url == '/' || req.url == '/news' || req.url == '/events') {
+   if (req.url == '/news') {
       res.write(`Please specify parameters`);
    }
 
