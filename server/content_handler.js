816290f7e5ea38b25691990a01d83d9970d8afc8
Griffin Ferguson <griffinferguson.us@gmail.com>
2022-07-25 00:11:35 -0500

    server: update(scripts): Continue work on content handler

--- server/content_handler.js
+++ server/content_handler.js
@@ -1,29 +1,41 @@
 const http = require('http');
 const mongo = require('mongodb');
-const dbUrl = 'mongodb://localhost:5000/content';
-const pug = require("pug")
+const dbUrl = 'mongodb://localhost:5000/';
+const pug = require("pug");
 
 console.log('Content Handler running on port 3001');
 
 // Connect content database
 const client = new mongo.MongoClient(dbUrl);
-var db = { 
-   events: client.collection('events'), 
-   news: client.collection('news')
+await client.connect()
+const database = client.db('content')
+const db = { 
+   events: database.collection('events'), 
+   news: database.collection('news')
 };
 
 // Compile templates
 const templates = {
    events: pug.compileFile('./templates/event.pug'),
    news: pug.compileFile('./templates/news.pug')
-}
+};
 
 
 http.createServer((req, res) => {
    res.writeHead(200, {'Content-type': 'application/json'});
 
-   if (req.url == '/news') {
-      res.write(`Please specify parameters`);
+   var cat = req.url.split('/')[1]
+
+   if (cat == "news") {
+      var content = await db.news.findOne({ id: req.url.split('/')[2] })
+      var article = templates.news({
+         title: content.title,
+         description: content.desc,
+         content: content.content,
+         date: content.pub
+      })
+
+      res.write(article)
    }
 
    res.end();
--- server/start.sh
+++ server/start.sh
@@ -1,8 +1,13 @@
 #!/usr/bin/env bash
 nginx
 echo "nginx started..."
-npx pm2 start db.js
-echo "Databases awaiting to be initialized"
+if [$(ls /data/db) != ""]
+then
+    npx pm2 start db.js
+    echo "Databases awaiting to be initialized"
+else
+    echo "Databases already exist, skipping initialization"
+fi
 npx pm2 start api.js
 npx pm2 start content_handler.js
 echo "APIs started"
