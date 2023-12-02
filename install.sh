2b26150fffdb0426e04cbbdfef09300791aa5ea6
Griffin Ferguson <griffinferguson.us@gmail.com>
2023-12-02 17:05:29 -0600

    various fixes and changes, work on additional artwork

--- install.sh
+++ install.sh
@@ -3,11 +3,12 @@
 ACTIVE_DIR=$PWD
 
 mkdir /var/www /var/www/html /var/www/server /var/www/shipyard
-mv ./server/nginx.conf /etc/nginx/nginx.conf
-mv ./client/dist /var/www/html
-mv ./server/pages /var/www/server
-git clone https://github.com/LandstriderLegion/coriolis.git /var/www/shipyard/
-cd /var/www/shipyard
+cp ./server/nginx.conf /etc/nginx/nginx.conf
+cp ./client/dist/ /var/www/html
+cp ./server/pages/ /var/www/server
+git clone https://github.com/LandstriderLegion/coriolis.git /var/www/shipyard/coriolis
+git clone https://github.com/LandstriderLegion/coriolis-data.git /var/www/shipyard/coriolis-data
+cd /var/www/shipyard/coriolis
 npm install
 npm run build
 cd $ACTIVE_DIR
